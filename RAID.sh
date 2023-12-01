d18e64105b3c5ea8a6df949d336bb17a1c73decf
Tybery <149952468+Tybery@users.noreply.github.com>
2023-12-01 13:34:14 +0300

    RAID.sh

--- RAID.sh
+++ RAID.sh
@@ -1,5 +1,5 @@
-mdadm --zero-superblock --force /dev/sd{a,b,c,d,e}
-mdadm --create --verbose /dev/md0 -l 5 -n 5 /dev/sd{a,b,c,d,e}
+mdadm --zero-superblock --force /dev/sd{b,c,d,e,f}
+mdadm --create --verbose /dev/md0 -l 6 -n 5 /dev/sd{b,c,d,e,f}
 mkdir /etc/mdadm
 echo "DEVICE partitions" > /etc/mdadm/mdadm.conf
 mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >> /etc/mdadm/mdadm.conf
